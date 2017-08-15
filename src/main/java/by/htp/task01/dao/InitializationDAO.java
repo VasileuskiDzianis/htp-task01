e71e6dd2422f5340940cec50948cddda6c5c91bf
Dzianis Vasileuski <d.vasileuski@gmail.com>
2017-08-15 16:51:35 +0300

    interface was deleted

--- src/main/java/by/htp/task01/dao/InitializationDAO.java
+++
@@ -1,8 +0,0 @@
-package by.htp.task01.dao;
-
-import by.htp.task01.dao.exception.DAOException;
-
-public interface InitializationDAO {
-	void initialize() throws DAOException;
-	void destroy() throws DAOException;
-}
--- src/main/java/by/htp/task01/service/InitializationService.java
+++
@@ -1,9 +0,0 @@
-package by.htp.task01.service;
-
-import by.htp.task01.service.exception.ServiceException;
-
-public interface InitializationService {
-	
-	void initialize() throws ServiceException;
-	void destroy() throws ServiceException;
-}
